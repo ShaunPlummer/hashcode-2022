424a53e303bd066c235bcfe9b96ea3aa8f8ffd63
doYouSpeakFish <ed_sills@hotmail.co.uk>
2022-02-24 18:28:47 +0000

    parse file

--- src/main/kotlin/Contributor.kt
+++ src/main/kotlin/Contributor.kt
@@ -2,4 +2,13 @@ data class Contributor(val name: String, val skills: List<Skill> = emptyList())
 
 data class Skill(val name: String, val level: Int)
 
-data class Project(val name: String, val daysToComplete:Int, val score:Int, val bestBeforeDay: Int, val numOfRoles:Int)
+data class Project(
+    val name: String,
+    val daysToComplete: Int,
+    val score: Int,
+    val bestBeforeDay: Int,
+    val numOfRoles: Int,
+    val roles: List<Role>
+)
+
+data class Role(val skillName: String, val level: Int)
